e8b2e0f0005513f3cc540753e39e43f985826c57
Victor Dupuy <victor@skillup.co>
2018-04-11 12:47:57 +0200

    refactored server.js + added file to gitignore

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var logger = require('./tools/logger');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const logger = require('./tools/logger');
 
-var app = express();
+const app = express();
 
 //Middlewares
 app.use(logger);
 app.use(express.static(__dirname + '/public'));
 
 //Routes
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  if(process.env.PORT){
-    console.log('API is running on port 3000 or '+process.env.PORT);
-  }else{
-    console.log('API is running on port 3000');
-  }
+app.listen(process.env.PORT || 9999, () => {
+  console.log(`Game is running on port ${process.env.PORT || 9999}`)
 });
