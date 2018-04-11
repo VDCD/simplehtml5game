503ad9e46a638f95eba69274be3ea62bb54926b5
Victor Dupuy <victor@skillup.co>
2018-04-11 12:50:00 +0200

    refactored logger.js + added nodemon for hot reloading

--- tools/logger.js
+++ tools/logger.js
@@ -1,15 +1,15 @@
 //This is a logger, giving whats sent from server to user and how much time it took.
-module.exports = function(request, response, next){
-    var start = +new Date();
-    var stream = process.stdout;
-    var url = request.url;
-    var methode = request.method;
+module.exports = (request, response, next) => {
+  const start = +new Date();
+  const stream = process.stdout;
+  const url = request.url;
+  const methode = request.method;
 
-    response.on('finish', function(){
-      var duration = +new Date() - start;
-      var message = methode + ' to ' +url + '\ntook '+ duration + ' ms \n\n';
-      stream.write(message); // Ecris le message.
-    });
+  response.on('finish', function () {
+    const duration = +new Date() - start;
+    const message = methode + ' to ' + url + '\ntook ' + duration + ' ms \n\n';
+    stream.write(message); // Ecris le message.
+  });
 
-    next();
+  next();
 }
